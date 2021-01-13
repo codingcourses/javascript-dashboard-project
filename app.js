a0aed1f7d4de6bf0efe83c86632daefa94f4bba8
Eric Liu <ericliu314@gmail.com>
2021-01-12 22:41:12 -0500

    Add GET /quote endpoint

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const quotes = require('./quotes.json');
+
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -8,6 +10,15 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+app.get('/quote', (req, res) => {
+  const index = randomIntFromInterval(0, quotes.length - 1);
+  res.send({ data: quotes[index] });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
